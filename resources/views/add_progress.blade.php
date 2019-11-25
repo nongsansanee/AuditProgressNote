678984a0471cd9d8483e03b42b7009347994b695
nongsansanee <nongcomster@gmail.com>
2019-11-25 15:58:44 +0700

    form add progress note complete

--- resources/views/add_progress.blade.php
+++ resources/views/add_progress.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 @section('title','add doctor')
 @section('content')
+@if($message = Session::get('success'))
+        <div class="alert alert-success">
+            {{$message}}
+        </div>
+ @endif
 
+ @if($errors->any())
+        <div class="alert alert-danger">
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+
+                @endforeach
+        </div>
+@endif
 <div id="app">
  <!-- @{{ doctors }} -->
-    <form action="/add_progress">
+    <form action="/save_progress" medthod="POST">
+      <input type="hidden"  name="_token" value="{{ csrf_token()}}">
         <div class="form-row mt-5" style="background-color:#BFF484">
             <div class="col-lg-12 text-center">       
                 <label>บันทึก Progress Note</label>     
@@ -27,16 +41,17 @@
                     <label for="exampleInputPassword1">เลือกชื่อหอผู้ป่วย :</label>
             </div>
             <div class="col col-lg-4"> 
-                    <select id="list_ward" class="form-control" v-model="ward">                   
-                        <option value="1">อัษฎางค์ 6 เหนือ</option>
-                        <option value="2">อัษฎางค์ 6 ใต้</option>
-                        <option value="3">อัษฎางค์ 9 เหนือ</option>
-                        <option value="4">อัษฎางค์ 9 ใต้</option>
+                    <select id="list_ward" class="form-control" name="ward" v-model="ward"> 
+                    @foreach($wards as $ward)    
+                     
+                            <option value="{{$ward['id']}}">{{ $ward['name']}}</option>
+                         
+                    @endforeach
                     </select>
            
             </div>
         </div>
-        <div class="row  mt-3">
+        <div class="row  mt-3" >
                 <table id="DoctorTable" class="table table-striped">
                     <thead>
                             <tr class="text-center">
@@ -59,7 +74,7 @@
                                  @{{doctor.line}}
                             </td>
                             <td class="text-center">
-                               <input class="form-check-input" type="radio" name="doctor_patient">
+                               <input class="form-check-input" type="radio" name="doctor_patient" :value="doctor.id">
                             </td>
                         </tr>
                             
@@ -84,24 +99,252 @@
             <div class="col col-lg-6">    
                 <label   v-text="`วันและเวลาที่ ADMIT : ${patient.datetime_admit}`"></label>  
             </div>
+       
         </div>
+
+        <div class="row"  style="background-color:#CAC9C3" v-if="patient.his_progress_note >0" >
+             <div class="col col-lg-6">    
+                <label  for="exampleRadios1">
+                    พบข้อมูลการตรวจสอบ progress note ดังนี้
+                </label>
+            </div>
+        </div>
+
+        <div class="row"  style="background-color:#CAC9C3" v-if="patient.his_progress_note >0" >
+          <div class="col col-lg-12 col-sm-12">    
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">ชื่อผู้ Audit</th>
+                    <th scope="col">วันที่ Audit</th>
+                    <th scope="col">พบ/ไม่พบ progressnote</th>
+                    <th scope="col">ชื่อแพทย์</th>
+                    <th scope="col">วันที่แพทย์เขียน Progress Note</th>
+                    <th scope="col">สถานะ Progress Note</th>
+                    <th scope="col">หมายเหตุ(Not Complete)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>น.ส.วนิดา ใจงาม</td>
+                        <td>15-10-2019</td>
+                        <td>พบ</td>
+                        <td>นายณเดช คุณกิติยะ</td>
+                        <td>11-10-2019</td>
+                        <td>Not Complete</td>
+                        <td>xxxxxxxx</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                        <td>น.ส.วนิดา ใจงาม</td>
+                        <td>20-10-2019</td>
+                        <td>พบ</td>
+                        <td>นายณเดช คุณกิติยะ</td>
+                        <td>19-10-2019</td>
+                        <td>Not Complete</td>
+                        <td>xxxxxxxx</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                        <td>น.ส.วนิดา ใจงาม</td>
+                        <td>30-10-2019</td>
+                        <td>ไม่พบ</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
+        </div>
+
+
         <div class="row"  style="background-color:#D3F3D7"  v-if="patient.length !== 0">
              <div class="col col-lg-3">    
-                <input type="radio" name="exampleRadios" id="pg-note"  value="1">
+                <input type="radio" name="checkNote" v-model="checkNote" value="1">
                 <label  for="exampleRadios1">
                     พบการเขียน progress note
                 </label>
             </div>
             <div class="col col-lg-3">    
-                <input  type="radio" name="exampleRadios" id="pg-note" value="2" >
+                <input  type="radio" name="checkNote" v-model="checkNote" value="2" >
                 <label class="form-check-label" for="exampleRadios1">
                     ไม่พบการเขียน progress note
                 </label>
             </div>
         </div>
 
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkNote == 1 ">
+             <div class="col col-lg-6">    
+                <label  for="exampleRadios1">
+                    วันที่แพทย์เขียน Progress Note:
+                </label>
+                <input type="date" name="dateWrite"  >
+            </div>
+        </div>
+
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkNote == 1 ">
+             <div class="col col-lg-3">    
+                <input type="checkbox" name="writeUnitOther"  value="1">
+                <label  for="exampleRadios1">
+                    เขียนโดยหน่วยอื่น
+                </label>
+            </div>
+        </div>
+
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkNote == 1 ">
+             <div class="col col-lg-3">    
+                <input type="checkbox" name="writeResident" v-model="checkWriteResident" value="2">
+                <label  for="exampleRadios1">
+                    เขียนโดย resident ward
+                </label>
+            </div>
+            <div class="col col-lg-3" v-if="checkWriteResident == true ">    
+                <input type="radio" name="residentID"  value="1">
+                <label  for="exampleRadios1">
+                    มีเลข ว และอ่านออก
+                </label>
+            </div>
+            <div class="col col-lg-3" v-if="checkWriteResident == true ">    
+                <input type="radio" name="residentID"  value="2">
+                <label  for="exampleRadios1">
+                    มีเลข ว แต่อ่านไม่ออก
+                </label>
+            </div>
+            <div class="col col-lg-3" v-if="checkWriteResident == true ">    
+                <input type="radio" name="residentID"  value="3">
+                <label  for="exampleRadios1">
+                    ไม่มีเลข ว 
+                </label>
+            </div>
+        </div>
+
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkNote == 1 ">
+             <div class="col col-lg-3">    
+                <input type="checkbox" name="writeExtern" v-model="checkWriteExtern"  value="3">
+                <label  for="exampleRadios1">
+                    เขียนโดย Extern
+                </label>
+            </div>
+            <div class="col col-lg-3" v-if="checkWriteExtern == true ">    
+                <input type="radio" name="MDSign"  v-model="checkMDSign" value="1">
+                <label  for="exampleRadios1">
+                    มี MD sign
+                </label>
+            </div>
+            <div class="col col-lg-3" v-if="checkWriteExtern == true ">    
+                <input type="radio" name="MDSign" v-model="checkMDSign" value="2">
+                <label  for="exampleRadios1">
+                    ไม่มี MD sign
+                </label>
+            </div>
+        </div>
         
-        <div class="row text-center mt-5" id="btn-save">
+        
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkMDSign == 1 && checkNote == 1 ">
+             <div class="col col-lg-3">    
+              
+                <label  for="exampleRadios1">
+                  
+                </label>
+            </div>
+            <div class="col col-lg-3">    
+                <input type="radio" name="mdID"  value="1">
+                <label  for="exampleRadios1">
+                    มีเลข ว และอ่านออก
+                </label>
+            </div>
+            <div class="col col-lg-3">    
+                <input type="radio" name="mdID"  value="2">
+                <label  for="exampleRadios1">
+                    มีเลข ว แต่อ่านไม่ออก
+                </label>
+            </div>
+            <div class="col col-lg-3">    
+                <input type="radio" name="mdID"  value="3">
+                <label  for="exampleRadios1">
+                    ไม่มีเลข ว 
+                </label>
+            </div>
+        </div>
+
+
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkNote == 1 ">
+             <div class="col col-lg-9">    
+                <input type="checkbox" name="writeOther" v-model="checkWriteOther"  value="4">
+                <label  for="exampleRadios1">
+                    เขียนโดยผู้อื่น โปรดระบุ
+                </label>
+                <input type="text" name="writeOtherDetail" size="50">          
+            </div>
+           
+        </div>
+        
+        
+        <div class="row"  style="background-color:#D3F3D7"  v-if="checkWriteOther == true && checkNote == 1 ">
+             <div class="col col-lg-3">    
+                <label  for="exampleRadios1">
+                </label>
+            </div>
+            <div class="col col-lg-3">    
+                <input type="radio" name="otherID"  value="1">
+                <label  for="exampleRadios1">
+                    มีเลข ว และอ่านออก
+                </label>
+            </div>
+            <div class="col col-lg-3">    
+                <input type="radio" name="otherID"  value="2">
+                <label  for="exampleRadios1">
+                    มีเลข ว แต่อ่านไม่ออก
+                </label>
+            </div>
+            <div class="col col-lg-3">    
+                <input type="radio" name="otherID"  value="3">
+                <label  for="exampleRadios1">
+                    ไม่มีเลข ว 
+                </label>
+            </div>
+        </div>
+
+        <div class="row"  style="background-color:#9BDAA5"  v-if="checkNote == 1 ">
+             <div class="col col-lg-3"> 
+                <label  for="exampleRadios1">
+                   สรุป Progress Note
+                </label>   
+            </div>
+            <div class="col col-lg-3"> 
+                <input type="radio" name="completeNote" v-model="completeNote" value="1" >
+                <label  for="exampleRadios1">
+                    complete
+                </label>
+            </div>
+            <div class="col col-lg-3"> 
+                <input type="radio" name="completeNote" v-model="completeNote" value="2"  >
+                <label  for="exampleRadios1">
+                    not complete โปรดระบุ
+                </label>
+            </div>
+        </div>
+        
+
+        <div class="row"  style="background-color:#9BDAA5; height: 80px"  v-if="checkNote == 1 && completeNote==2 ">
+             <div class="col col-lg-3"> 
+                <label  for="exampleRadios1">
+                
+                </label>   
+            </div>
+            <div class="col col-lg-3"> 
+                
+            </div>
+            <div class="col col-lg-6"> 
+                <textarea class="form-control" id="completeNoteDetail" rows="2"></textarea>
+            </div>
+        </div>
+
+        <div class="row text-center mt-5" id="btn-save" v-if="completeNote !== 0 || checkNote == 2">
             <div class="col col-lg-12 ">  
                  <button type="submit" class="btn btn-primary">บันทึก</button>
             </div>
@@ -121,15 +364,18 @@ var app = new Vue({
             ward: null,
             an: null,
             patient: [],
-            // doctors:[
-            //         {id:1 ,name:"นายณเดช คุณกิติยะ"},
-            //         {id:2 ,name:"นางศันสนีย์ คุณกิติยะ"}
-            //         ],
-
+            checkNote:0,
+            checkWriteResident:0,
+            checkWriteExtern:0,
+            checkMDSign:0,
+            checkWriteOther:0,
+            completeNote:0
+          
             
         },
         mounted(){
              document.querySelector("#list_ward").addEventListener("change", this.addDoctor);
+            
         },
         methods:{
             addDoctor() {
@@ -147,7 +393,7 @@ var app = new Vue({
             searchPatient(){
                 console.log(this.an);
                 axios.post('/get-patient', {
-                    an: this.an,
+                    an_patient: this.an,
                 })
                 .then((response) => {
                     this.patient = response.data;
@@ -156,7 +402,9 @@ var app = new Vue({
                 .catch((error) => {
                     console.log(error);
                 });                
-            }
+            },
+        
+
         }
 
 });
--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,79 @@ Route::get('/show_doctor', function () {
 });
 
 Route::get('/add_progress', function () {
-    return view('add_progress');
+    $wards[] = ['id'=>1,'name'=>'อัษฎางค์ 6 เหนือ'];
+    $wards[] = ['id'=>2,'name'=>'อัษฎางค์ 6 ใต้'];
+    $wards[] = ['id'=>3,'name'=>'อัษฎางค์ 9 เหนือ'];
+    $wards[] = ['id'=>4,'name'=>'อัษฎางค์ 9 ใต้'];
+
+    return view('add_progress')->with(['wards'=>$wards]);
 });
 
 Route::post('/get-doctors', function(){
-    $doctors[] =  [ 'id' => 1 ,'name' => 'นายณเดช คุณกิติยะ' ];
-    $doctors[] = [ 'id' => 2 ,'name' => 'นางศันสนีย์ คุณกิติยะ' ];
-    return $doctors;
+
+    $ward_id =  request()->input('ward_id');
+    $current_date=getdate();
+    $date_search = "$current_date[mday]-$current_date[mon]-$current_date[year]";
+
+    Log::debug($ward_id);
+    Log::debug($date_search);
+    if($ward_id == '1'){
+        $doctors[] =  [ 'id' => 1 ,'name' => 'นพ.ณเดช คุณกิติยะ'   ];
+        $doctors[] = [ 'id' => 2 ,'name' => 'พญ.ศันสนีย์ คุณกิติยะ' ];
+        return $doctors;
+    }
+
+        $doctors[] =  [ 'id' => 3 ,'name' => 'นพ.วัชระ คนดี'   ];
+        $doctors[] = [ 'id' => 4 ,'name' => 'นพ.อาคม มหาศาล' ];
+        return $doctors;
 });
 
 Route::post('/get-line-doctors', function(){
-    //  return $ward_id;
-    $doctors[] =  [ 'id' => 1 ,'name' => 'นายณเดช คุณกิติยะ','line' => 'A' ];
-    $doctors[] = [ 'id' => 2 ,'name' => 'นางศันสนีย์ คุณกิติยะ','line' => 'B' ];
+  
+    $ward_id =  request()->input('ward_id');
+    if($ward_id == '1'){
+        $doctors[] =  [ 'id' => '123456' ,'name' => 'นพ.ณเดช คุณกิติยะ','line' => 'A' ];
+        $doctors[] = [ 'id' => '234567' ,'name' => 'พญ.ศันสนีย์ คุณกิติยะ','line' => 'B' ];
+        return $doctors;
+    }
+    $doctors[] =  [ 'id' => '345678' ,'name' => 'นพ.วัชระ คนดี','line' => 'A' ];
+    $doctors[] = [ 'id' => '456789' ,'name' => 'นพ.อาคม มหาศาล','line' => 'B' ];
     return $doctors;
 });
 
 Route::post('/get-patient', function(){
-    //  return $ward_id;
-    // $patient[] =  [ 'id' => 1 ,'patient_name' => 'นางสาวอุรัสยา จงใจดี','datetime_admit' => '11-10-2019' ];
-    $patient =  [ 'id' => 1 ,'patient_name' => 'นางสาวอุรัสยา จงใจดี','datetime_admit' => '11-10-2019 06:11:00' ];
+   
+   
+    // Log::debug("test write log");
+    // Log::debug(request()->input('an_patient'));
+
+    $an =  request()->input('an_patient');
+    // Log::debug($an);
+
+    if($an == '123'){
+        $patient =  [ 'id' => 1 ,'patient_name' => 'นางสมศรี น้ำใจมาก','datetime_admit' => '02-10-2019 06:11:00','his_progress_note'=>'3'];
+        return $patient;
+    }
+  
+    $patient =  [ 'id' => 1 ,'patient_name' => 'นางสาวอุรัสยา จงใจดี','datetime_admit' => '11-10-2019 06:11:00','his_progress_note'=>'0'];
 
     return $patient;
+});
+
+Route::get('/save_progress',function(Illuminate\Http\Request $request){
+
+    $validate = [
+        'doctor_patient' => 'required',
+      
+    ];
+
+    $errorMsg = [
+        'doctor_patient.required' => 'กรุณาเลือกแพทย์',
+    ];
+
+    $request->validate($validate,$errorMsg);
+
+    return redirect()->back()->with('success','Create Successfully');
+     return $request->all();  
+    
 });
